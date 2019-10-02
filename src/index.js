e700483e79c705b2594930895c6405c8306f3750
DaniAkash <s.daniakash@gmail.com>
2019-10-02 22:07:11 +0530

    get height and width inside the fuction call

--- src/index.js
+++ src/index.js
@@ -22,21 +22,20 @@
  * SOFTWARE.
  */
 
-import {
-  Dimensions
-} from 'react-native';
+import { Dimensions } from "react-native";
 
-const {height, width} = Dimensions.get('window');
-
-export const responsiveHeight = (h) => {
-  return height*(h/100);
+export const responsiveHeight = h => {
+  const { height } = Dimensions.get("window");
+  return height * (h / 100);
 };
 
-export const responsiveWidth = (w) => {
-  return width*(w/100);
+export const responsiveWidth = w => {
+  const { width } = Dimensions.get("window");
+  return width * (w / 100);
 };
 
-export const responsiveFontSize = (f) => {
-  const tempHeight = (16/9)*width;
-  return Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(width, 2))*(f/100);
+export const responsiveFontSize = f => {
+  const { width } = Dimensions.get("window");
+  const tempHeight = (16 / 9) * width;
+  return Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(width, 2)) * (f / 100);
 };
