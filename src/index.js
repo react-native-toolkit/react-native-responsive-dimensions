11319a4c406d81818766816c67bf37177cd7e40b
DaniAkash <s.daniakash@gmail.com>
2019-10-02 22:40:33 +0530

    create responsive hooks

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@
  * SOFTWARE.
  */
 
+import { useState, useEffect } from "react";
 import { Dimensions } from "react-native";
 
 export const responsiveHeight = h => {
@@ -38,4 +39,57 @@ export const responsiveFontSize = f => {
   const { width } = Dimensions.get("window");
   const tempHeight = (16 / 9) * width;
   return Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(width, 2)) * (f / 100);
+};
+
+export const useResponsiveHeight = h => {
+  const [calculatedHeight, setCalculatedHeight] = useState(responsiveHeight(h));
+
+  useEffect(() => {
+    function handleDimensionChange() {
+      setCalculatedHeight(responsiveHeight(h));
+    }
+
+    Dimensions.addEventListener("change", handleDimensionChange);
+    return () => {
+      Dimensions.removeEventListener("change", handleDimensionChange);
+    };
+  });
+
+  return calculatedHeight;
+};
+
+export const useResponsiveWidth = w => {
+  const [calculatedWidth, setCalculatedWidth] = useState(responsiveWidth(w));
+
+  useEffect(() => {
+    function handleDimensionChange() {
+      setCalculatedWidth(responsiveWidth(w));
+    }
+
+    Dimensions.addEventListener("change", handleDimensionChange);
+    return () => {
+      Dimensions.removeEventListener("change", handleDimensionChange);
+    };
+  });
+
+  return calculatedWidth;
+};
+
+export const useResponsiveFontSize = f => {
+  const [calculatedFontSize, setCalculatedFontSize] = useState(
+    responsiveFontSize(f)
+  );
+
+  useEffect(() => {
+    function handleDimensionChange() {
+      setCalculatedFontSize(responsiveFontSize(f));
+    }
+
+    Dimensions.addEventListener("change", handleDimensionChange);
+    return () => {
+      Dimensions.removeEventListener("change", handleDimensionChange);
+    };
+  });
+
+  return calculatedFontSize;
 };
