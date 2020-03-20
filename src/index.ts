986b8c9c708f6fcb898b9d5a0e043d3a0f498002
DaniAkash <s.daniakash@gmail.com>
2020-03-20 10:24:27 +0530

    added new methods

---
+++ src/index.ts
@@ -0,0 +1,162 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-present DaniAkash
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { useState, useEffect, useRef } from "react";
+import { Dimensions, ScaledSize } from "react-native";
+
+const useDimensionsListener = () => {
+  const [screenDimension, setScreenDimension] = useState(
+    Dimensions.get("screen")
+  );
+  const [windowDimension, setWindowDimension] = useState(
+    Dimensions.get("window")
+  );
+
+  useEffect(() => {
+    function handleDimensionChange({
+      window,
+      screen
+    }: {
+      window: ScaledSize;
+      screen: ScaledSize;
+    }) {
+      setWindowDimension(window);
+      setScreenDimension(screen);
+    }
+
+    Dimensions.addEventListener("change", handleDimensionChange);
+    return () => {
+      Dimensions.removeEventListener("change", handleDimensionChange);
+    };
+  }, []);
+
+  return {
+    screen: screenDimension,
+    window: windowDimension
+  };
+};
+
+type EffectParams = {
+  screen: ScaledSize;
+  window: ScaledSize;
+};
+
+type EffectCallback =
+  | ((opts: EffectParams) => () => any)
+  | ((opts: EffectParams) => undefined)
+  | ((opts: EffectParams) => void);
+
+export const useDimensionsChange = (effect: EffectCallback) => {
+  const hasMountRef = useRef(false);
+  const dimensions = useDimensionsListener();
+
+  useEffect(() => {
+    if (hasMountRef.current) {
+      const destroy = effect(dimensions);
+      let cleanUp: any = () => null;
+      if (typeof destroy === "function") {
+        cleanUp = destroy;
+      }
+      return () => cleanUp();
+    } else {
+      hasMountRef.current = true;
+    }
+  }, [dimensions, hasMountRef]);
+};
+
+export const responsiveHeight = (h: number) => {
+  const { height } = Dimensions.get("window");
+  return height * (h / 100);
+};
+
+export const responsiveWidth = (w: number) => {
+  const { width } = Dimensions.get("window");
+  return width * (w / 100);
+};
+
+export const responsiveFontSize = (f: number) => {
+  const { height, width } = Dimensions.get("window");
+  const lowerSize = height > width ? width : height;
+  const tempHeight = (16 / 9) * lowerSize;
+  return (
+    Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(lowerSize, 2)) * (f / 100)
+  );
+};
+
+export const responsiveScreenHeight = (h: number) => {
+  const { height } = Dimensions.get("screen");
+  return height * (h / 100);
+};
+
+export const responsiveScreenWidth = (w: number) => {
+  const { width } = Dimensions.get("screen");
+  return width * (w / 100);
+};
+
+export const responsiveScreenFontSize = (f: number) => {
+  const { height, width } = Dimensions.get("screen");
+  const lowerSize = height > width ? width : height;
+  const tempHeight = (16 / 9) * lowerSize;
+  return (
+    Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(lowerSize, 2)) * (f / 100)
+  );
+};
+
+export const useResponsiveHeight = (h: number) => {
+  const { height } = useDimensionsListener().window;
+  return height * (h / 100);
+};
+
+export const useResponsiveWidth = (w: number) => {
+  const { width } = useDimensionsListener().window;
+  return width * (w / 100);
+};
+
+export const useResponsiveFontSize = (f: number) => {
+  const { height, width } = useDimensionsListener().window;
+  const lowerSize = height > width ? width : height;
+  const tempHeight = (16 / 9) * lowerSize;
+  return (
+    Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(lowerSize, 2)) * (f / 100)
+  );
+};
+
+export const useResponsiveScreenHeight = (h: number) => {
+  const { height } = useDimensionsListener().screen;
+  return height * (h / 100);
+};
+
+export const useResponsiveScreenWidth = (w: number) => {
+  const { width } = useDimensionsListener().screen;
+  return width * (w / 100);
+};
+
+export const useResponsiveScreenFontSize = (f: number) => {
+  const { height, width } = useDimensionsListener().screen;
+  const lowerSize = height > width ? width : height;
+  const tempHeight = (16 / 9) * lowerSize;
+  return (
+    Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(lowerSize, 2)) * (f / 100)
+  );
+};
